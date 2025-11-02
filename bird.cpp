5047d111d81fa1be2f5a2845603342bd1d5eb68f
김진경 <maine0948@o.cnu.ac.kr>
2025-11-02 20:45:32 +0900

    과제 제출

--- bird.cpp
+++ bird.cpp
@@ -1,3 +1,48 @@
 #include <iostream>
 #include "bird.h"
 
+class Duck : public Bird {
+public:
+    Duck() : Bird("Duck") {
+        cry();
+    }
+    void cry() {
+        std::cout << "Quack!" << std::endl;
+    }
+    virtual ~Duck() {
+        cry();
+    }
+};
+
+class Penguin : public Bird {
+public:
+    Penguin() : Bird("Penguin") {
+        cry();
+    }
+    void cry() {
+        std::cout << "Squawk!" << std::endl;
+    }
+    void fly() {
+        std::cout << "This bird can't fly." << std::endl;
+    }
+    virtual ~Penguin() {
+        cry();
+    }
+};
+
+class Eagle : public Bird {
+public:
+    Eagle() : Bird("Eagle") {
+        cry();
+    }
+    void cry() {
+        std::cout << "Screech!" << std::endl;
+    }
+    void swim() {
+        std::cout << "This bird can't swim." << std::endl;
+    }
+    virtual ~Eagle() {
+        cry();
+    }
+};
+
